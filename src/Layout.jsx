e40d35b91c3e82b97014f0be78da103fc3486596
12-vanshika-04 <vanshikajain1204@gmail.com>
2026-05-21 18:05:32 +0530

    footer issue resolved

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,7 @@
 // src/Layout.jsx
 import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { checkGoogleUser } from "./features/auth/authSlice";
